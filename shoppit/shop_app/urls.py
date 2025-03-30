6a11e2ff04a712d5ae596ce841c777a567858173
johnson <ibitoyejohnson234@gmail.com>
2025-03-30 11:32:51 +0100

    My E-commerce Backend

--- shoppit/shop_app/urls.py
+++ shoppit/shop_app/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path("products/", views.products, name="products"),
     path("product_detail/<slug:slug>", views.product_detail, name="product_detail"),
     path("add_item/", views.add_item, name="add"),
-    path("product_in_cart", views.product_in_cart, name="product_in_cart")
+    path("product_in_cart", views.product_in_cart, name="product_in_cart"),
+    path("get_cart_stat", views.get_cart_stat, name="get_cart_stat")
 ]
 
 # fetching all_products: http://127.0.0.1:8001/products
