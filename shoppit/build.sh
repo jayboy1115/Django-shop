71ba6434e23fdd6a7580d66b581865cac0b429d2
jayboy1115 <ibitoyejohnson234@gmail.com>
2025-05-22 16:18:58 +0100

    My E-Commerce Backend

--- shoppit/build.sh
+++ shoppit/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -o errexit
 
-pip install -r requirements.txt  # ← Fixed (added 's')
+# Install dependencies using Poetry
+poetry install --no-interaction --no-ansi
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
